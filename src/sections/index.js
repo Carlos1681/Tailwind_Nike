1627d0412daf116721ecac32a55a353521d2802b
Carlos1681 <spjcarlosh@gmail.com>
2024-05-01 20:47:00 -0300

    Initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from './CustomerReviews'
+import Footer from './Footer'
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import SpecialOffers from './SpecialOffers'
+
+export {
+    Hero,
+    Footer,
+    CustomerReviews,
+    PopularProducts,
+    Services,
+    Subscribe,
+    SpecialOffers,
+    SuperQuality
+}
